2d86b812cd1b9de1fec37bfc84c2dc488bd40135
parth696 <parth.12282@gmail.com>
2016-12-01 19:18:46 +0530

    handlebars blah blah

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@ $(document).ready(function (){
     $('.slider').slider({full_width: true});
     $('.modal-trigger').leanModal();
     $(".button-collapse").sideNav();
+    $('.collapsible').collapsible();
+    var TechData=Handlebars.templates['TechnicalCommittee'](technicalCommitteeData);
+    $('#technical-committee').html(TechData);
+
 });
---
+++ js/technicalCommiteeData.js
@@ -0,0 +1,37 @@
+/**
+ * Created by user on 01-12-2016.
+ */
+var technicalCommitteeData={
+    "tech":[
+        {
+            "dept":"maths",
+            "content":[
+                {"name":"Prof(Dr.) D C Vakaskar",
+                "des":"Professor Head, MSU"
+                },
+                {"name":"Prof.R.K.George ",
+                "des":"Professor,Dept.of Mathematics, IIST Trivandrum"
+                },
+                {"name":"Prof. N. Sukavanam",
+                "des":"Professor, Dept of Mathematics, IIT Roorkee"
+                }
+            ]
+        },
+        {
+            "dept":"it",
+            "content":[
+                {"name":"Prof(Dr.) D C Vakaskar",
+                    "des":"Professor Head, MSU"
+                },
+                {"name":"Prof.R.K.George ",
+                    "des":"Professor,Dept.of Mathematics, IIST Trivandrum"
+                },
+                {"name":"Prof. N. Sukavanam",
+                    "des":"Professor, Dept of Mathematics, IIT Roorkee"
+                }
+            ]
+        }
+    ]
+
+
+};
