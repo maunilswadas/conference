8ee610e80e6301efc9526a27ec49df1b07f0305a
parth696 <parth.12282@gmail.com>
2016-12-02 11:10:26 +0530

    Maths & EE Data

--- js/technicalCommiteeData.js
+++ js/technicalCommiteeData.js
@@ -4,7 +4,7 @@
 var technicalCommitteeData={
     "tech":[
         {
-            "dept":"maths",
+            "dept":"MATHS/HS",
             "content":[
                 {"name":"Prof(Dr.) D C Vakaskar",
                 "des":"Professor Head, MSU"
@@ -14,21 +14,143 @@ var technicalCommitteeData={
                 },
                 {"name":"Prof. N. Sukavanam",
                 "des":"Professor, Dept of Mathematics, IIT Roorkee"
+                },
+                {
+                    "name":"Dr G M Deheri Rader ",
+                    "des":"Dept. of Mathematics, SPU, VV Nagar"
+                },
+                {
+                    "name":"Dr V D Pathak Director ",
+                    "des":"Academy of creativity and Excellence, Vadodara"
+                },
+                {
+                    "name":"Dr V.N.Mishra ",
+                    "des":"Assi. Prof SVNIT, SURAT"
+                },
+                {
+                    "name":"Dr. R. D. Mehta Professor ",
+                    "des":"Dept. of Mathematics, SPU"
+                },
+                {
+                    "name":"Dr A H Hasmani ",
+                    "des":"Professor &amp; Head, SPU"
+                },
+                {
+                    "name":"Dr A K Shukla ",
+                    "des":"Associate Professor, SVNIT, SURAT"
+                },
+                {
+                    "name":"Prof K A Thaker ",
+                    "des":"Associate Professor, GU"
+                },
+                {
+                    "name":"Dr. Sulabha Natraj ",
+                    "des":"Principal, Waymade College of Education"
+                },
+                {
+                    "name":"Dr P K Priyan  ",
+                    "des":"Professor, Institute of Mgt , SPU"
+                },
+                {
+                    "name":"Dr K S Prasad ",
+                    "des":"Asso Prof, Institute of Mgt , SPU"
+                },
+                {
+                    "name":"Dr. Shailesh N Hadli ",
+                    "des":"Principal; R N P I School of Law &amp; Justice, VVN"
                 }
+
             ]
         },
         {
-            "dept":"it",
+            "dept":"EE",
             "content":[
-                {"name":"Prof(Dr.) D C Vakaskar",
-                    "des":"Professor Head, MSU"
+                {"name":"Dr. S. K. Joshi",
+                    "des":"MSU, Vadodara"
                 },
-                {"name":"Prof.R.K.George ",
-                    "des":"Professor,Dept.of Mathematics, IIST Trivandrum"
+                {"name":"Dr. H. R. Jariwala ",
+                    "des":"SVNIT,SURAT"
                 },
-                {"name":"Prof. N. Sukavanam",
-                    "des":"Professor, Dept of Mathematics, IIT Roorkee"
+                {"name":"Dr. Sanjay R. Joshi",
+                    "des":"GEC, Valsad"
+                },
+                {
+                    "name":"Dr. Vivek Pandya ",
+                    "des":"School of Technology, PDPU, Gandhinagar"
+                },
+                {
+                    "name":"Dr. P. N. Tekwani  ",
+                    "des":"Institute of Technology, Nirma University"
+                },
+                {
+                    "name":"Dr. B. R. Parekh  ",
+                    "des":"B.V.M. Engineering College"
+                },
+                {
+                    "name":"Dr. N. G. Mishra  ",
+                    "des":"B.V.M. Engineering College"
+                },
+                {
+                    "name":"Dr. Akshay A. Pandya  ",
+                    "des":"B.V.M. Engineering College"
+                },
+                {
+                    "name":"Dr. Gopal Gajjar" ,
+                    "des":"PowerAnser Lab,IIT, Bombay"
+                },
+                {
+                    "name":"Dr. Yogesh Bichpuriya  ",
+                    "des":"Tata Consultancy Services,Pune"
+                },
+                {
+                    "name":"Dr. Anandita Chaudhary  ",
+                    "des":"SVNIT, Surat"
+                },
+                {
+                    "name":"Dr. A. K. Panchal   ",
+                    "des":"SVNIT, Surat"
+                },
+                {
+                    "name":"Dr. Hiren Patel  ",
+                    "des":"SCET, Surat"
+                },
+                {
+                    "name":"Dr. Ketan P. Badgujar",
+                    "des":"SSGEC, Bhavnagar"
+                },
+                {
+                    "name":"Dr. Bhavik N. Suthar  ",
+                    "des":"GEC, Modasa"
+                },
+                {
+                    "name":"Dr. Mahipalsinh Chudasama  ",
+                    "des":"LDCE, Ahmedabad"
+                },
+                {
+                    "name":"Dr. Axay J. Mehta",
+                    "des":"IITRAM, Ahmedabad"
+                },
+                {
+                    "name":" Dr. Ritesh R. Patel",
+                    "des":"GCET, V. V. Nagar"
+                },
+                {
+                    "name":"Dr. Vijay H. Makwana",
+                    "des":"GCET, V. V. Nagar"
+                },
+                {
+                    "name":"Dr. Pragnesh H. Shah",
+                    "des":"ADIT, V. V. Nagar"
+                },
+                {
+                    "name":"Dr. Pragnesh Bhatt",
+                    "des":"CSPIT, Charusat University, Changa"
+                },
+                {
+                    "name":"Dr. Kartik S. Pandya",
+                    "des":"CSPIT, Charusat University, Changa"
                 }
+
             ]
         }
     ]
